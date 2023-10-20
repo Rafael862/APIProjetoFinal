daab26ba9002b26bc97eb174d147425f51c539e4
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-10-20 07:16:44 -0300

    buscando erro de validação de email

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -25,23 +25,23 @@ class UserController{
         if(!user){
             throw new AppError("Usuário não encontrado!");
         }
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
-
-        if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
-            throw new AppError("Este email já está em uso!");
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+            throw new AppError("Este email já está em uso.");
         }
-
-        user.name = name;
+ 
+        user.name = name;  
         user.email = email;
         
         await database.run(`
         UPDATE users SET 
         name = ?, 
         email = ?, 
-        updated_at = ?, 
+        updated_at = ? 
         WHERE id = ?`, 
-        [user.name, user.email, new Date(), id]);
+        [user.name, user.email, new Date(), id]
+        );
         return response.json();
     }
 }
