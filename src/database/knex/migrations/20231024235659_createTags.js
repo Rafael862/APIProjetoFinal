f49a026a57663c2b5e5bcdb2c751aa3ff4b656ed
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-10-30 21:36:18 -0300

    Finalizado o backend

--- src/database/knex/migrations/20231024235659_createTags.js
+++ src/database/knex/migrations/20231024235659_createTags.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable("tags", table => {
     table.increments("id");
     table.text("name").notNullable();
-    table.text("description");
+    
     table.integer("user_id").references("id").inTable("users");
     table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
   });
