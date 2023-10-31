a543240a0104735777c87b9da9a4e9443160538d
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-10-31 20:50:44 -0300

    foi automatizado a criação do user admin

--- src/database/sqlite/migrations/createUsers.js
+++ src/database/sqlite/migrations/createUsers.js
@@ -6,6 +6,18 @@ email VARCHAR,
 password VARCHAR,
 avatar VARCHAR NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)`;
-module.exports = createUsers;   
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+isAdmin BOOLEAN
+);
+
+INSERT INTO users (name, email, password, avatar, created_at, updated_at, isAdmin)
+SELECT 'admin', 'admin', '123', null, '31/10/2023', '31/10/2023', true
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM users
+    WHERE name = 'admin'  
+)
+
+`;
+
+module.exports = createUsers;    
