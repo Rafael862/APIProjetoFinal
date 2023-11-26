88fd97d660fda0a2513c97de9753a3230ab374b9
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-11-26 10:09:03 -0300

    Revisado todo o back-end

--- src/server.js
+++ src/server.js
@@ -1,28 +1,37 @@
-require("express-async-errors");
-const express = require('express');
-const routes = require("./routes");
-const app = express();
-const AppError = require("./utils/AppError");
-//const database = require("./database/sqlite");
-const migrationsRun = require("./database/sqlite/migrations");
+require('dotenv/config');
+require('express-async-errors');
 
+const express = require('express');
+
+const AppError = require('./utils/AppError');
+const routes = require('./routes');
+const uploadConfig = require('./configs/upload');
+
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
-migrationsRun();
-//database();
-app.use((error, request, response, next) => {
-    if(error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        });
-    } 
-    console.error(error);
-    return response.status(500).json({
-        status: "error",
-        message: "internal server error"
+
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
     });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
 });
 
-const PORT =  3333;
-app.listen(PORT, () => console.log(`O servidor está rodando na porta: ${PORT}`));
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
